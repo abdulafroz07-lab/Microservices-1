fad812eb4b1afe51125be81dca5518b7e9f243de
Abdul AfrOz <abdulafroz07@gmail.com>
2026-03-17 23:54:04 +0530

    Delete Afroz directory

--- Afroz/order-service/src/main/java/com/amazon/order_service/Repositories/OrderRepo.java
+++
@@ -1,9 +0,0 @@
-package com.amazon.order_service.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.amazon.order_service.entity.Order;
-
-public interface OrderRepo extends JpaRepository<Order,Integer> {
-
-}
